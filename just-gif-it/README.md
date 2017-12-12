37f527fac10754c636ec9c8fe6eeeb49a66d6e71
shaikhwa <shaikhwa@MUMWD101061.ASIAPAC.NOM>
2017-12-12 12:38:29 +0530

    Second Commit

---
+++ just-gif-it/README.md
@@ -0,0 +1,9 @@
+# Reduce App Restart time
+ ```
+<!-- Reduces Restart time -->
+		<dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-devtools</artifactId> 
+			<version>1.3.3.RELEASE</version>
+		</dependency>
+```
--- just-gif-it/src/main/java/shaikh/waqar/io/controller/UploadController.java
+++ just-gif-it/src/main/java/shaikh/waqar/io/controller/UploadController.java
@@ -1,5 +1,12 @@
 package shaikh.waqar.io.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,17 +15,27 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+@RestController()
 public class UploadController {
+	
+	private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	
+	@Value("${multipart.location}")
+	private String location;
 
 	@RequestMapping(value="/upload", method=RequestMethod.POST, produces = MediaType.IMAGE_GIF_VALUE)
 	public String upload(@RequestPart("file") MultipartFile file,
 			@RequestParam("start") int start,
 			@RequestParam("end") int end,
 			@RequestParam("speed") int speed,
-			@RequestParam("repeat") boolean repeat){
+			@RequestParam("repeat") boolean repeat) throws IllegalStateException, IOException{
 		
+		File videoFile = new File(location + "/" + System.currentTimeMillis() + ".mp4");
 		
-				return null;
+		file.transferTo(videoFile);
+		
+		log.info("Saved ile to {}", videoFile.getAbsolutePath());
+		
+		return null;
 	}
 }
